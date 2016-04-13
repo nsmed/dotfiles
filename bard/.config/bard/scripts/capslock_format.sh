052cc163ed519a633a2cc80cc0781b1225328fb7
Nikolaj <nikolajsmed@gmail.com>
2016-04-13 17:08:59 +0200

    Changed caps, num, and bard depth

--- bard/.config/bard/scripts/capslock_format.sh
+++ bard/.config/bard/scripts/capslock_format.sh
@@ -1,5 +1,5 @@
 if [[ "$1" = "on" ]]; then
-	echo -en "%{F\$xcolor[green]}%{T\$font[icon]}\uE02F"
+	echo -en "%{F\$xcolor[blue]}%{T\$font[icon]}\uE02F"
 else
 	echo -en "%{F\$xcolor[grey]}%{T\$font[icon]}\uE02F"
 fi
--- bard/.config/bard/scripts/numlock_format.sh
+++ bard/.config/bard/scripts/numlock_format.sh
@@ -1,5 +1,5 @@
 if [[ "$1" = "on" ]]; then
-	echo -en "%{F\$xcolor[green]}%{T\$font[icon]}\uE187"
+	echo -en "%{F\$xcolor[blue]}%{T\$font[icon]}\uE187"
 else
 	echo -en "%{F\$xcolor[grey]}%{T\$font[icon]}\uE187"
 fi
